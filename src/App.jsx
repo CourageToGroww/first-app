5cabfcf400bd5c73dee36916d3e98adda56c07bf
CourageToGroww <Monkeysbuymainstream@gmail.com>
2023-03-07 23:30:21 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className="todo">
+      
+      </div>
+      <h1>Our First App</h1>
+      <div className="card">
+
+        <p>
+
+        </p>
+      </div>
+      <p className="read-the-docs">
+        
+      </p>
+    </div>
+  )
+}
+
+export default App
